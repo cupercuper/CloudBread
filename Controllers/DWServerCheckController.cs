9f4744224bbdfdde62839dd5116e2b7646449c60
Victory <mkmk8846@gmail.com>
2017-11-16 18:54:02 +0900

    쿼리문 수정

--- Controllers/DWServerCheckController.cs
+++ Controllers/DWServerCheckController.cs
@@ -111,7 +111,7 @@ namespace CloudBread.Controllers
             RetryPolicy retryPolicy = new RetryPolicy<SqlAzureTransientErrorDetectionStrategy>(globalVal.conRetryCount, TimeSpan.FromSeconds(globalVal.conRetryFromSeconds));
             using (SqlConnection connection = new SqlConnection(globalVal.DBConnectionString))
             {
-                string strQuery = string.Format("SELECT * FROM ServerCheck");
+                string strQuery = string.Format("SELECT CheckStartTime, CheckEndTime FROM ServerCheck");
                 using (SqlCommand command = new SqlCommand(strQuery, connection))
                 {
                     connection.OpenWithRetry(retryPolicy);
