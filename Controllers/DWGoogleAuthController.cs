122b340e3f5069e506741fb05b42010c7a30741a
Victory <mkmk8846@gmail.com>
2017-12-25 23:28:21 +0900

    구글 인증 토큰 추가

---
+++ Controllers/DWGoogleAuthController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace CloudBread.Controllers
+{
+    [MobileAppController]
+    public class DWGoogleAuthController : ApiController
+    {
+        // GET api/DWGoogleAuth
+        public string Get()
+        {
+            return "Hello from custom controller!";
+        }
+
+        // GET api/DWGoogleAuth
+        public IHttpActionResult Index(string code)
+        {
+            return null;
+        }
+
+        public string Post()
+        {
+            return "Hello from custom controller Post!";
+        }
+
+
+    }
+}
